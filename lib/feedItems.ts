a79e1cb3f390b75d3c6af042f5aa07db8dca1262
artemiui <bonniearcega@gmail.com>
2025-12-28 02:18:21 +0800

    Update feedItems.ts

--- lib/feedItems.ts
+++ lib/feedItems.ts
@@ -2,17 +2,19 @@ import { getAllPosts } from "./mdx";
 import { FeedItemType } from "@/components/FeedItem";
 
 // External links that don't have MDX files
-const externalLinks: Omit<FeedItemType, "category">[] = [
+const externalLinks: FeedItemType[] = [
   {
     title: "A Computational Model of Piano Finger Technique",
     date: "2023-12-15",
     url: "https://nlp.lab.uic.edu/wp-content/uploads/sites/314/2023/06/Randolph_CRF2.pdf",
     description: "An interesting paper on modeling piano fingering using CRF. Initial thoughts of innovation given features is that we can optimize for teaching piano finger arrangement much easier. Also, accessibility.",
+    category: "Knowledge",
   },
   {
     title: "The Most Beautiful Line in a Video Game",
     date: "2025-5-05",
     url: "https://www.youtube.com/watch?v=UKU4YYOHdk0",
+    category: "Media",
     description: "A video I made earlier this year on UNDERTALE's iconic line that despite everything, it's still you.",
   },
 ];
@@ -30,10 +32,7 @@ export async function getFeedItems(): Promise<FeedItemType[]> {
   }));
 
   // Add external links with their categories
-  const externalItems: FeedItemType[] = externalLinks.map((link) => ({
-    ...link,
-    category: "Knowledge" as const, // Default category for external links
-  }));
+  const externalItems: FeedItemType[] = externalLinks;
 
   // Combine and sort by date (newest first)
   const allItems = [...postItems, ...externalItems].sort((a, b) => {
