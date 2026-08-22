4a73bb9c3e2f4b71fe8eac0250847ec3ec7a177b
artemiui <bonniearcega@gmail.com>
2026-08-22 11:51:54 +0800

    Update category of 'Analytic Philosophy in the Philippines' to Knowledge Sharing

--- lib/feedItems.ts
+++ lib/feedItems.ts
@@ -111,7 +111,7 @@ const externalLinks: FeedItemType[] = [
     title: "Analytic Philosophy in the Philippines",
     date: "2026-08-21",
     url: "https://doi.org/10.1007/s44204-022-00029-z",
-    category: "Research",
+    category: "Knowledge Sharing",
     tags: ["Philosophy"],
     description: "What I think to be the initial reading for the Philosophical Society of UP Diliman. I had recently joined regardless of being a major in statistics with my withstanding love for the department and for analytic philosophy in particular.",
   },
